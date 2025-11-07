c2ba2b8d1560f7c8bfcd5ec3d6b8975707c41529
Architmaurya <architmaurya7054@gmail.com>
2025-11-07 17:10:55 +0530

    save

---
+++ JavaFirstPro/Demo.java
@@ -0,0 +1,8 @@
+package JavaFirstPro;
+
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println(args[0]);
+    }
+    
+}
--- JavaFirstPro/Main.java
+++ JavaFirstPro/Main.java
@@ -2,6 +2,6 @@ package JavaFirstPro;
 
 public class Main  {
     public static void main (String[] args){
-        System.out.println("Hello World!");
+        System.out.println("Archit maurya ");
     }
 }
