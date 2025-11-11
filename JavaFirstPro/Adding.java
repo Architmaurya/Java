803d979d518de7fdf7d9b483abc6e9949aab0f02
Architmaurya <architmaurya7054@gmail.com>
2025-11-11 16:06:20 +0530

    add

---
+++ JavaFirstPro/Adding.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Adding {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println ("Enter the number ");
+        int num1 =input.nextInt();
+        int num2 =input.nextInt();
+        int sum =num1 + num2;
+        System.out.println("the sum of the two number is " +sum );
+    }
+}
---
+++ JavaFirstPro/San.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class San {
+    public static void main(String[] args) {
+        // System.out.println("Hey how are you ");
+        Scanner input = new Scanner(System.in);
+        System.out.print(input.nextLine());
+    }
+}
