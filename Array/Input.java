6336abb7af938f770bb91f81383945f31cddb170
Architmaurya <architmaurya7054@gmail.com>
2025-11-16 21:29:11 +0530

    about the arrays

---
+++ Array/Input.java
@@ -0,0 +1,36 @@
+package Array;
+import java.util.Arrays;
+
+import java.util.Scanner;
+
+public class Input {
+ public static void main (String[] args){
+    Scanner in =new Scanner(System.in);
+    // arrays of premivites
+    // int [] arr = new int [5];
+    // arr[0]=55;
+    // arr[1]=44;
+    // arr[2]=33;
+    // arr[3]=22;
+    // arr[4]=99;
+    // System.out.println(arr[3]);
+    // for(int i=0 ; i< arr.length ; i++){
+    //     arr[i]=in.nextInt();
+    // }
+    // System.out.println(Arrays.toString(arr));
+    // for (int i = 0 ;i < arr.length ;i++){
+    //     // System.out.println(arr[i]);
+        
+    // }
+    // array of object 
+    String[] str= new String[4];
+    for (int i =0 ; i<str.length; i++){
+        str[i]=in.next();
+
+    }
+    System.out.println(Arrays.toString(str));
+    //modify
+    str[2]="Archit";
+    System.out.println(Arrays.toString(str));
+ }   
+}
---
+++ Array/Main.java
@@ -0,0 +1,25 @@
+package Array;
+
+public class Main {
+    public static void main (String [] args){
+        // Q store a roll nummber
+        // int a =19;
+         //Q: store a person 's name '
+        // String name = "Archit maurya";
+
+
+        //suntax of the array
+        //datatype[] cariable_name =new datatpye[size]
+        // store 5 roll number 
+        // int [] rnos= new int [5];
+        // //or directly 
+        int [] ro = new int[5];
+        // System.out.println (ro[0]);
+         
+        String [] nu = new String[5];
+        System.out.println (nu[0]);
+
+
+         
+    }
+}
