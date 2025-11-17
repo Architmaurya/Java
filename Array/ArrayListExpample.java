e14357fcab3a4bb35dde31a44a4389b6ab6f5bdf
Architmaurya <architmaurya7054@gmail.com>
2025-11-17 14:26:56 +0530

    ArrayList

--- Array/ArrayListExpample.java
+++ Array/ArrayListExpample.java
@@ -1,5 +1,35 @@
 package Array;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class ArrayListExpample {
+  public static void main (String[] args){
+
+    Scanner in =new Scanner(System.in);
+      //Syntax
+      ArrayList<Integer>list = new ArrayList<>(10);
+    //   list.add(10);
+    //   list.add(29);
+    //    list.add(13);
+    //   list.add(22);
+    //    list.add(133);
+    
+    //   list.set(1,100);
+
+    //   System.out.println(list.contains(1045));
+    //   System.out.println(list);
+
+      for (int i= 0 ;i<5 ; i++){
+        list.add(in.nextInt());
+      }
+      //get the item at any index have to pass the index over there 
+
+      for (int i=0; i<5;i++){
+        System.out.println(list.get(i));//pass index here ,list [index] syntax will not work here 
+      }
+
+
+  }
+
     
 }
--- Array/startpart.java
+++ Array/startpart.java
@@ -1,19 +1,16 @@
 package Array;
 
 public class startpart {
-    public static void main (String[] args){
+    public static void main(String[] args) {
         // Scanner in = new Scanner(System.in);
-        int [][] arr = new int [4][];
-        for (int i=0;i<arr.length;i++){
-            for (int j=0 ;j<arr.length-i-1;j++){
-                System.out.print(" ");
-            } 
-            for (int j=0;j<2*i+1;j++)
-           {
-              System.out.print("*");
-           }
-            System.out.println();
+        int[] jum = { 1, 2, 3, 4, 5 };
+        for (int i = 0; i < jum.length; i++) {
+            System.out.println(jum[i]);
         }
-       
+
     }
 }
+
+
+// string = 1234554321
+// true and false
